211cb7dbc165bc4526fe305095d46232cfad66c4
Rafel Siregar <50065143+rafelsiregar@users.noreply.github.com>
2025-05-18 23:30:11 +0700

    Add backend/src/main/java/com/elearning/dto/QuizDTO.java 1747585809450

---
+++ backend/src/main/java/com/elearning/dto/QuizDTO.java
@@ -0,0 +1,33 @@
+package com.elearning.dto;
+
+import java.util.List;
+
+public class QuizDTO {
+    private Long id;
+    private String title;
+    private List<QuestionDTO> questions;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<QuestionDTO> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<QuestionDTO> questions) {
+        this.questions = questions;
+    }
+}
