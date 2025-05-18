05027110967b39f86e231ffcef96c7e6b26440a8
Rafel Siregar <50065143+rafelsiregar@users.noreply.github.com>
2025-05-18 23:30:27 +0700

    Add backend/src/main/java/com/elearning/repository/QuizRepository.java 1747585824545

---
+++ backend/src/main/java/com/elearning/repository/QuizRepository.java
@@ -0,0 +1,9 @@
+package com.elearning.repository;
+
+import com.elearning.model.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
+}
