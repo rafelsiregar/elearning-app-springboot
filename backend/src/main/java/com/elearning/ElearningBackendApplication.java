ff1b00c2896ea8e0157e28ed7ab7eb7563ca0a52
Rafel Siregar <50065143+rafelsiregar@users.noreply.github.com>
2025-05-18 23:30:33 +0700

    Add backend/src/main/java/com/elearning/ElearningBackendApplication.java 1747585831116

---
+++ backend/src/main/java/com/elearning/ElearningBackendApplication.java
@@ -0,0 +1,12 @@
+package com.elearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearningBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElearningBackendApplication.class, args);
+    }
+}
